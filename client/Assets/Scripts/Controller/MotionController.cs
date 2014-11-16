1cf8b1dd865a1962020e1cb2ad2e2f1f998f53e7
Leonardo <kroltan1@gmail.com>
2014-11-15 23:47:19 -0200

    client/Assets/Scripts/Controller/MotionController.cs: * added multi-jump * controller/Unity Input support * long-jumping

--- client/Assets/Scripts/Controller/MotionController.cs
+++ client/Assets/Scripts/Controller/MotionController.cs
@@ -1,17 +1,23 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class MotionController : MonoBehaviour {
 
-	public float moveSpeed = 1f;
-	public float maxVelocity = 1f;
-	public float jumpSpeed = 1f;
-	public float jumpMovementMultiplier = 1f;
+	public float moveSpeed = 1f,
+				 maxVelocity = 1f,
+				 jumpSpeed = 1f,
+				 extraJumpSpeedMultiplier = 0.5f,
+				 jumpTimeout = 1f,
+				 extendedJumpDuration = 0.7f,
+				 jumpMovementMultiplier = 1f,
+				 jumpCollisionCompensation = 0.1f;
 	public int maximumJumpCount = 1;
 
 	private float jumpStartTime;
 	private int jumpCount;
+	private bool wasJumpingBefore;
 
 	void Start() {
 		
@@ -23,11 +29,17 @@ public class MotionController : MonoBehaviour {
 
 		var jumpingKey = Input.GetAxis("Jump") > 0.5f;
 		if (jumpingKey) {
-			Jump();
+			if (!wasJumpingBefore && jumpStartTime + jumpTimeout < Time.timeSinceLevelLoad) {
+				Jump(jumpCount == 0? 1.0f : extraJumpSpeedMultiplier);
+			}
+			if (jumpStartTime + extendedJumpDuration > Time.timeSinceLevelLoad) {
+				rigidbody2D.AddForce(Vector2.up * jumpSpeed * jumpSpeed);
+			}
 		}
+		wasJumpingBefore = jumpingKey;
 	}
 
-	void InfluenceMovement (float hInput) {
+	void InfluenceMovement(float hInput) {
 		var adjustedMoveSpeed = hInput * moveSpeed * Helpers.TimeScale;
 		if (jumpCount > 0) {
 			adjustedMoveSpeed *= jumpMovementMultiplier;
@@ -40,19 +52,27 @@ public class MotionController : MonoBehaviour {
 	}
 
 	private void Jump() {
+		Jump(1.0f);
+	}
+
+	private void Jump(float multiplier) {
 		if (jumpCount < maximumJumpCount) {
 			jumpCount++;
 			jumpStartTime = Time.timeSinceLevelLoad;
 			if (rigidbody2D.velocity.y < 0) {
 				rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0);
 			}
-			rigidbody2D.velocity += new Vector2(0, jumpSpeed);
+			rigidbody2D.velocity += new Vector2(0, jumpSpeed * multiplier);
 		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.gameObject.tag.Equals("Ground")) {
-			jumpCount = 0;
+			bool isFeetTouching = collision.contacts.Select(
+				c => c.normal).All(p => Vector2.Dot(p, Vector2.up) > 0.5f);
+			if (isFeetTouching) {
+				jumpCount = 0;
+			}
 		}
 	}
 }
--- client/Assets/Scripts/Helpers.cs
+++ client/Assets/Scripts/Helpers.cs
@@ -7,5 +7,15 @@ public class Helpers {
 			return Time.timeScale * Time.deltaTime;
 		}
 	}
+
+	public static void DrawRect(Vector3 p0, Vector3 p3, Color color, float duration) {
+		var p1 = new Vector3(p0.x, p3.y);
+		var p2 = new Vector3(p3.x, p0.y);
+		
+		Debug.DrawLine(p0, p1, color, duration);
+		Debug.DrawLine(p1, p3, color, duration);
+		Debug.DrawLine(p3, p2, color, duration);
+		Debug.DrawLine(p2, p0, color, duration);
+	}
 }
 
